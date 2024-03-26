4f0657a1ec8bafb162582f82ff2016a236e443aa
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-03-26 08:39:41 +0000

    responsiveness for mobile

--- src/components/UserProfileData.js
+++ src/components/UserProfileData.js
@@ -1,17 +1,16 @@
-// UserProfileData.js
 import lucieImage from './images/square.png';
 
 const userProfiles = [
-  { id: 1, name: 'Natasha', age: 26, imageUrl: lucieImage, bio: 'Natasha thrives in creative chaos, a visionary artist who paints her dreams.' },
-  { id: 2, name: 'Olivia', age: 22, imageUrl: lucieImage, bio: 'Olivia, a tech enthusiast, explores the digital world with boundless curiosity.' },
-  { id: 3, name: 'Eve', age: 19, imageUrl: lucieImage, bio: 'Eve is a budding poet, finding beauty in the mundane, turning it into verse.' },
-  { id: 4, name: 'Emily', age: 21, imageUrl: lucieImage, bio: 'Emily, an aspiring chef, blends tradition and innovation in her delightful dishes.' },
-  { id: 5, name: 'Liz', age: 25, imageUrl: lucieImage, bio: 'Liz, a spirited activist, passionately advocates for environmental conservation.' },
-  { id: 6, name: 'Emma', age: 31, imageUrl: lucieImage, bio: 'Emma, a dedicated nurse, provides care with compassion and resilience.' },
-  { id: 7, name: 'Amelia', age: 29, imageUrl: lucieImage, bio: 'Amelia, an adventurous traveler, seeks the stories hidden in uncharted lands.' },
-  { id: 8, name: 'Erin', age: 24, imageUrl: lucieImage, bio: 'Erin, a gifted musician, expresses her soul through the strings of her violin.' },
-  { id: 9, name: 'Connie', age: 18, imageUrl: lucieImage, bio: 'Connie, a fearless skateboarder, rides the edge between adrenaline and freedom.' },
-  { id: 10, name: 'Amy', age: 22, imageUrl: lucieImage, bio: 'Amy, a keen astronomer, gazes at the stars, dreaming of distant worlds.' },
+  { id: 1, name: 'Natasha', age: 26, imageUrl: lucieImage, bio: 'Natasha thrives in creative chaos, a visionary artist who paints her dreams.', tags: ['Creative', 'Visionary', 'Artistic'] },
+  { id: 2, name: 'Olivia', age: 22, imageUrl: lucieImage, bio: 'Olivia, a tech enthusiast, explores the digital world with boundless curiosity.', tags: ['Tech-savvy', 'Curious', 'Innovative'] },
+  { id: 3, name: 'Eve', age: 19, imageUrl: lucieImage, bio: 'Eve is a budding poet, finding beauty in the mundane, turning it into verse.', tags: ['Poetic', 'Observant', 'Inspired'] },
+  { id: 4, name: 'Emily', age: 21, imageUrl: lucieImage, bio: 'Emily, an aspiring chef, blends tradition and innovation in her delightful dishes.', tags: ['Culinary Artist', 'Innovative', 'Passionate'] },
+  { id: 5, name: 'Liz', age: 25, imageUrl: lucieImage, bio: 'Liz, a spirited activist, passionately advocates for environmental conservation.', tags: ['Activist', 'Passionate', 'Eco-Conscious'] },
+  { id: 6, name: 'Emma', age: 31, imageUrl: lucieImage, bio: 'Emma, a dedicated nurse, provides care with compassion and resilience.', tags: ['Compassionate', 'Dedicated', 'Resilient'] },
+  { id: 7, name: 'Amelia', age: 29, imageUrl: lucieImage, bio: 'Amelia, an adventurous traveler, seeks the stories hidden in uncharted lands.', tags: ['Adventurous', 'Storyteller', 'Explorer'] },
+  { id: 8, name: 'Erin', age: 24, imageUrl: lucieImage, bio: 'Erin, a gifted musician, expresses her soul through the strings of her violin.', tags: ['Musical', 'Expressive', 'Gifted'] },
+  { id: 9, name: 'Connie', age: 18, imageUrl: lucieImage, bio: 'Connie, a fearless skateboarder, rides the edge between adrenaline and freedom.', tags: ['Fearless', 'Adventurous', 'Skateboarder'] },
+  { id: 10, name: 'Amy', age: 22, imageUrl: lucieImage, bio: 'Amy, a keen astronomer, gazes at the stars, dreaming of distant worlds.', tags: ['Astronomer', 'Dreamer', 'Curious'] },
 ];
 
 export default userProfiles;
